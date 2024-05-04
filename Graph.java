2adbaa0f9af388c76e40eea627571e87cb738cdd
MohammedAhmmed <mohammedahmmed4920@gmail.com>
2024-05-04 22:42:29 +0300

    add floyd warshall

--- Graph.java
+++ Graph.java
@@ -5,13 +5,19 @@ import java.util.PriorityQueue;
 public class Graph {
 
     ArrayList<ArrayList<Pair<Integer, Integer>>> adjlist=new ArrayList<ArrayList<Pair<Integer, Integer>>>();
+    private long[][] adjMatrix;
+    private final int vertices;
+    private final ArrayList<edge>edges;
     public Graph(int vertices,ArrayList<edge>edges,String way)
     {
+        this.vertices = vertices;
+        this.edges = edges;
          if(way.equals("Dijkstra"))
          {
              creategraph1(vertices,edges);
-
          }
+         if(way.equals("Floyd"))
+             graphInitialize();
     }
     private void creategraph1(int vertices,ArrayList<edge> edges)
     {
@@ -59,6 +65,81 @@ public class Graph {
             visited[entry[0]]=true;
         }
     }
+    public boolean floyd_warshall(long[][]distances, int[][]next){
+        initiate(distances, next);
+        for(int k = 0; k < vertices; k++){
+            for(int i = 0; i < vertices; i++){
+                for(int j = 0; j < vertices; j++){
+                    if((distances[i][k] != Integer.MAX_VALUE && distances[k][j] != Integer.MAX_VALUE) && (distances[i][k] + distances[k][j] < distances[i][j])) {
+                        distances[i][j] = distances[i][k] + distances[k][j];
+                        next[i][j] = next[i][k];
+                    }
+                }
+            }
+        }
+        return negativeCycleCheck(distances, next);
+    }
+    public long floydWarshallFindDistance(int source, int destination, long[][]distances){
+        return distances[source][destination];
+    }
+    public ArrayList<Integer> floydWarshallFindPath(int source, int destination, long[][] distances, int[][]next){
+        if(next[source][destination] == -1 || distances[source][destination] == Integer.MIN_VALUE || distances[source][destination] == Integer.MAX_VALUE)
+            return null;
 
+        ArrayList<Integer> path = new ArrayList<>();
+        int nextNode = source;
+        while(nextNode != destination){
+            path.add(nextNode);
+            nextNode = next[nextNode][destination];
+            if(nextNode == -1)
+                return null;
+        }
+        if(next[nextNode][destination] == -1)
+            return null;
+
+        path.add(nextNode);
+        return path;
+    }
+    private boolean negativeCycleCheck(long[][] distances, int[][]next){
+        boolean negativeCycle = false;
+        for(int k = 0; k < vertices; k++){
+            for(int i = 0; i < vertices; i++){
+                for(int j = 0; j < vertices; j++){
+                    if((distances[i][k] != Integer.MAX_VALUE && distances[k][j] != Integer.MAX_VALUE) && (distances[i][k] + distances[k][j] < distances[i][j])) {
+                        distances[i][j] = Integer.MIN_VALUE;
+                        next[i][j] = -1;
+                        negativeCycle = true;
+                    }
+                }
+            }
+        }
+        return negativeCycle;
+    }
+    private void initiate(long[][]distances, int[][]next){
+        distances = adjMatrix;
+        for(int i = 0; i < vertices; i++){
+            for(int j = 0; j < vertices; j++){
+                if(distances[i][j] != Integer.MAX_VALUE)
+                    next[i][j] = j;
+                else
+                    next[i][j] = -1;
+            }
+        }
+    }
+    private void graphInitialize(){
+        adjMatrix = new long[vertices][vertices];
+        for(int i = 0; i < vertices; i++){
+            for(int j = 0; j < vertices; j++) {
+                if(i == j)
+                    adjMatrix[i][j] = 0;
+                else
+                    adjMatrix[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (edge edge : edges) {
+            adjMatrix[edge.getFrom()][edge.getTo()] = edge.getWeight();
+        }
+    }
 
 }
